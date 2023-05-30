07110e66a50ad872d8acaf7fbc192c4153d0611c
Fedorov <chugun777akk@mail.ru>
2023-05-30 21:48:02 +0700

    i have trouble in method product_of_fractions

--- Practical_task_2/task_2.py
+++ Practical_task_2/task_2.py
@@ -5,12 +5,21 @@
 import fractions
 
 
-def product_of_fractions(str1: str, str2: str) -> int:
-    return 10
+def product_of_fractions(str1: str, str2: str):
+    list_number = str1.split('/')
+    a1 = int(list_number[0])
+    b1 = int(list_number[1])
+    list_number = str2.split('/')
+    a2 = int(list_number[0])
+    b2 = int(list_number[0])
+    numerator = (a1*a2)
+    denominator = (b1*b2)
+    return numerator / denominator
 
 
-def adding_fractions(str1: str, str2: str) -> int:
-    return 10
+
+def adding_fractions(str1: str, str2: str) -> float:
+    return None
 
 
 string_1 = str(input("Введите строку 1 вида a/b: "))
@@ -18,3 +27,6 @@ string_2 = str(input("Введите строку 2 вида a/b: "))
 print(f"Вы ввели {string_1} и {string_2} \n" \
       f"Сложение дробей {adding_fractions(string_1,string_2)} \n" \
       f"Произведение дробей {product_of_fractions(string_1,string_2)}")
+f1 = fractions.Fraction(string_1)
+f2 = fractions.Fraction(string_2)
+print(f1*f2)
