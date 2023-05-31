de3f02b0fdec29c8cd8cd891309c4f18ec1a1887
Fedorov <chugun777akk@mail.ru>
2023-05-31 09:13:39 +0700

    method adding_fractions is done

--- Practical_task_2/task_2.py
+++ Practical_task_2/task_2.py
@@ -6,30 +6,44 @@ import fractions
 import math
 
 
-def product_of_fractions(str1: str, str2: str) ->str:
+def product_of_fractions(str1: str, str2: str) -> str:
     list_number = str1.split('/')
     a1 = int(list_number[0])
     b1 = int(list_number[1])
     list_number = str2.split('/')
     a2 = int(list_number[0])
     b2 = int(list_number[1])
-    numerator = (a1*a2)
-    denominator = (b1*b2)
-    gcd = math.gcd(numerator,denominator)
+    numerator = (a1 * a2)
+    denominator = (b1 * b2)
+    gcd = math.gcd(numerator, denominator)
 
-    return f"{numerator//gcd}/{denominator//gcd}"
+    return f"{numerator // gcd}/{denominator // gcd}"
 
 
+def adding_fractions(str1: str, str2: str) -> str:
+    list_number = str1.split('/')
+    a1 = int(list_number[0])
+    b1 = int(list_number[1])
+    list_number = str2.split('/')
+    a2 = int(list_number[0])
+    b2 = int(list_number[1])
+    common_denominator = b1 * b2
+    additional_coefficient = common_denominator//b1
+    a1 *= additional_coefficient
+    additional_coefficient = common_denominator//b2
+    a2 *= additional_coefficient
 
-def adding_fractions(str1: str, str2: str) -> float:
-    return None
+    return f"{a1 + a2}/{common_denominator}"
 
 
 string_1 = str(input("Введите строку 1 вида a/b: "))
 string_2 = str(input("Введите строку 2 вида a/b: "))
-print(f"Вы ввели {string_1} и {string_2} \n" \
-      f"Сложение дробей {adding_fractions(string_1,string_2)} \n" \
-      f"Произведение дробей {product_of_fractions(string_1,string_2)}")
-f1 = fractions.Fraction(string_1)
+print(f"Вы ввели {string_1} и {string_2} \n" 
+      f"Сложение дробей {adding_fractions(string_1, string_2)} \n" 
+      f"Произведение дробей {product_of_fractions(string_1, string_2)}")
+
+"""f1 = fractions.Fraction(string_1)
 f2 = fractions.Fraction(string_2)
-print(f1*f2)
+print(f1 * f2)
+print(f1 + f2)
+"""
