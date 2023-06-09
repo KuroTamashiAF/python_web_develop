eb2e697d6bab51585ac25b232f931aafc543dbdf
Fedorov <chugun777akk@mail.ru>
2023-06-09 18:00:37 +0700

    task1 is done

--- Practical_task_4/task_1.py
+++ Practical_task_4/task_1.py
@@ -1,8 +1,11 @@
 """Напишите функцию для транспонирования матрицы"""
 
 
-def transposition(matrix: list) -> list:
-    pass
+def transposition(matrix: list[int]):
+    result = list(zip(*matrix))
+    return result
 
 
 matr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(transposition(matr))
