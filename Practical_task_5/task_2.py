a3afd4dcd0c43a88bae3c08e4e515b70bf97bc48
Fedorov <chugun777akk@mail.ru>
2023-06-12 12:04:38 +0700

    task_2 is done

--- Practical_task_5/task_2.py
+++ Practical_task_5/task_2.py
@@ -3,5 +3,17 @@
 В результате получаем словарь с именем в качестве ключа и суммой премии в качестве значения.
 Сумма рассчитывается как ставка умноженная на процент премии"""
 
+
+def crete_dict(names: list, bet: list, prize: list):
+    data = {key: value / 100 * perc for key, value, perc in zip(names, bet, prize)}
+    for k, v in data.items():
+        print(f"{k} - {v}")
+
+    return data
+
+
 names = ["Alex", "Yana", "Dasha", "Albert"]
-bet = [100000, 75000, 580000, 97000]
+bet = [100000, 75000, 58000, 97000]
+prize = [25.0, 20.53, 15.25, 22.5]
+
+crete_dict(names, bet, prize)
