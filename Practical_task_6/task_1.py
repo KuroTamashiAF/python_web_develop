4c79cd3cec9d49df2ef041409d6ba5b5a4468b3d
Fedorov <chugun777akk@mail.ru>
2023-06-16 20:03:22 +0700

    task_1 is done

--- Practical_task_6/task_1.py
+++ Practical_task_6/task_1.py
@@ -1,7 +1,9 @@
 """В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку."""
 
 
-def checking_leap(string: str) -> bool:
+def checking_leap(string: str = None) -> bool:
+    if string is None:
+        print("Вы нечего не передали")
     *_, year = [int(i) for i in string.split(".")]
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         return True
@@ -9,6 +11,5 @@ def checking_leap(string: str) -> bool:
         return False
 
 
-date = input("Введите дату в формате DD.MM.YYYY ")
-# print(checking_date(date))
-print(checking_leap(date))
+if __name__ == "__main__":
+    print(checking_leap("15.07.2000"))
